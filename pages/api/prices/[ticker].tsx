7cc1fdc877f23299bdb0478de7129ac2375dab96
Eduds007 <eduardomilanezaraujo@gmail.com>
2023-08-05 19:13:05 -0300

    add: prices endpoint

---
+++ pages/api/prices/[ticker].tsx
@@ -0,0 +1,27 @@
+
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+export default async (req, res) => {
+    
+    const { ticker }  = req.query
+    
+  try {
+    const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${ticker}.SA`);
+
+    const data = response.data.chart.result[0].meta;
+
+    const output = {
+        "currency":data.currency,
+        "symbol": data.symbol,
+        "price":data.regularMarketPrice,
+        "time": Date.now()
+    }
+
+    res.status(200).json(output);
+
+  } catch (error) {
+    console.error('Error fetching external data:', error);
+    res.status(500).json({ error: 'Failed to fetch external data' });
+  }
+};
